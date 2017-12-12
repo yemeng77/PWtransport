fe73b848069610fbeb7d4bc403f368d736e0350d
Meng Ye <yemeng77@gmail.com>
2017-12-11 16:28:05 -0800

    use mxc eigen states near Eref

--- SRC/PEtot_trans/PEtot_trans2.src/CG_linear.f
+++ SRC/PEtot_trans/PEtot_trans2.src/CG_linear.f
@@ -35,10 +35,13 @@ c       complex*16 workr_n(mg_nx)
 **********************************************
 **** if iopt=0 is used, pghh_old can be deleted
 **********************************************
-        integer lin_st(mst),m_max(mstateT)
-	real*8 E_st(mst),err_st(mst),eigen(mst)
-	real*8 Ef,occ(mst)
+       integer lin_st(mst),m_max(mstateT)
+       real*8 E_st(mst),err_st(mst),eigen(mst),eigen_diff(mst)
+       real*8 Ef,occ(mst)
        complex*16 Zbeta,Zpu
+
+       real*8, allocatable, dimension (:) :: eigen_sort
+       complex*16, allocatable, dimension (:,:) :: ug_n_sort
 
        common /com123b/m1,m2,m3,ngb,nghb,ntype,rrcut,msb
        common /comEk/Ek
@@ -48,6 +51,44 @@ c       complex*16 workr_n(mg_nx)
        cai=dcmplx(0.d0,1.d0)
 
        allocate(wgp_nh(mg_nx,mstateT))
+
+cONA       mxc=mx-10
+c       mxc=mx-mxlow      ! changed, lWW
+**** do not use the eigen state of H
+       mxc=15
+
+       do i=1,mst
+       eigen_diff(i)=dabs(eigen(i)-Eref)
+       enddo
+
+       allocate(eigen_sort(mst))
+       eigen_sort=eigen_diff
+       
+       do i=1,mst
+       do j=1,mst-1
+       if(eigen_sort(j).gt.eigen_sort(j+1)) then
+        temp=eigen_sort(j)
+        eigen_sort(j)=eigen_sort(j+1)
+        eigen_sort(j+1)=temp
+       endif
+       enddo
+       enddo
+
+       if(mxc.gt.mst) mxc=mst
+       if(mxc.lt.1) mxc=1
+       min=eigen_sort(mxc)
+
+       deallocate(eigen_sort)
+
+       do index=1,mst
+       if(eigen_diff(i).le.min) exit
+       enddo
+
+       allocate(eigen_sort(mxc))
+       allocate(ug_n_sort(ng_n,mxc))
+       eigen_sort=eigen(index:index+mxc-1)
+       ug_n_sort=ug_n(:,index:index+mxc-1)
+
 
 cccccccccccccccccccccccc
 
@@ -69,19 +110,12 @@ cccccccccccccccccccccccc
        wgp_nh(i,iii)=wgp_nh(i,iii)-Eref*wgp_n0(i,iii)
        enddo
 
-
-cONA       mxc=mx-10
-c       mxc=mx-mxlow      ! changed, lWW
-**** do not use the eigen state of H
-       mxc=0
-
-
-       call orth_comp_N(wgp_nh(1,iii),ug_n,mxc,2,kpt,Zcoeff(1,iii))
+       call orth_comp_N(wgp_nh(1,iii),ug_n_sort,mxc,2,kpt,Zcoeff(1,iii))
 
 cccccccccccccccccccccccccccccccccccccccccccccccc
 
         do m=1,mxc
-         dE=eigen(m)-Eref
+         dE=eigen_sort(m)-Eref
          if(dabs(dE).lt.1.D-20) dE=1.D-20
          Zcoeff(m,iii)=Zcoeff(m,iii)/dE**2
         enddo
@@ -301,7 +335,7 @@ cccccccccccccccccccccccccccccccccccccccccccc
 
       do m=1,mxc
       do i=1,ng_n
-      wgc_n(i)=wgc_n(i)-Zcoeff(m,iii)*ug_n(i,m)
+      wgc_n(i)=wgc_n(i)-Zcoeff(m,iii)*ug_n_sort(i,m)
       enddo
       enddo
 cccccccccccccccccccccccccccccccccccccc
@@ -354,7 +388,9 @@ c       endif
 4000  continue
 
 
-      deallocate(wgp_nh)
+       deallocate(wgp_nh)
+       deallocate(eigen_sort)
+       deallocate(ug_n_sort)
 ***********************************************
 
       return
