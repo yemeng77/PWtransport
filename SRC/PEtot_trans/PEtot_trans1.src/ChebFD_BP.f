404f890e1e73624f87f2d5ad217ba9462118f0ee
Meng Ye <yemeng77@gmail.com>
2018-01-18 21:45:21 -0800

    orthonormalize can work now, but the err_st is very large, need to check

--- SRC/PEtot_trans/PEtot_trans1.src/ChebFD_BP.f
+++ SRC/PEtot_trans/PEtot_trans1.src/ChebFD_BP.f
@@ -31,7 +31,7 @@ cc     See J. Comp. Phys. 325 (2016) 226–243
       real*8 E_st(mst),err_st(mst)
       real*8 E_m(nblock_band_mx),err_m(nblock_band_mx)
       real*8 tmp_real(nblock_band_mx),dumm(mst)
-      integer Np,Nlan,ikernel
+      integer Np,Nlan,ikernel,info
 
       real*8 alpha,beta,c0,g0
 
@@ -78,8 +78,6 @@ cc     See J. Comp. Phys. 325 (2016) 226–243
       write(6,*) "*********************************"
       endif
 
-101   format("[",2(f13.8,1x),"]")
-
 ****************************************
 cc    map the x in [Ebound(1),Ebound(2)] to [-1,1] using alpha*x+beta
 cc    alpha=2/(Ebound(2)-Ebound(1)), beta=(Ebound(1)+Ebound(2))/(Ebound(1)-Ebound(2))
@@ -92,8 +90,6 @@ cc    alpha=2/(Ebound(2)-Ebound(1)), beta=(Ebound(1)+Ebound(2))/(Ebound(1)-Eboun
       call mpi_abort(MPI_COMM_WORLD,ierr)
       endif
 
-      allocate(cn(Np))
-      allocate(gn(Np))
       sumdum_m=0.d0
       if(ipsp_all.eq.1) then
       allocate(sug_m(1,nblock_band_mx))
@@ -108,6 +104,8 @@ cc    W(x) is a windows function, which is 1 in [Ewind(1),Ewind(2)] and 0 outsid
 cc    W(x)\approx\sum_0^mp gn*cn*Tn(alpha*x+beta)
 cc    The filter: W(H)ug=\sum_0^mp gn*cn*Tn(alpha*H+beta)ug
 ****************************************
+      allocate(cn(Np))
+      allocate(gn(Np))
       pi=4.0d0*datan(1.0d0)
       c0=(dacos(alpha*Ewind(2)+beta)-dacos(alpha*Ewind(1)+beta))/pi
       g0=1.0d0
@@ -115,39 +113,18 @@ cc    The filter: W(H)ug=\sum_0^mp gn*cn*Tn(alpha*H+beta)ug
       do n=1,Np
       cn(n)=2.0d0*(dsin(n*dacos(alpha*Ewind(2)+beta))
      &     -dsin(n*dacos(alpha*Ewind(1)+beta)))/(n*pi)
-      if(ikernel.eq.-1) then
-        gn(n)=(Np-n+1.0d0)/(Np+1.0d0) !Fej\'er kernel
+      if(ikernel.gt.0) then
+        gn(n)=(dsin(n*pi/(Np+1))/(n*pi/(Np+1)))**ikernel !Lanczos kernel
       else if(ikernel.eq.0) then
         gn(n)=((Np-n)*dcos(n*pi/Np)+dsin(n*pi/Np)/dtan(pi/Np))/Np !Jackson kernel
-      else if(ikernel.gt.0) then
-        gn(n)=(dsin(n*pi/(Np+1.0d0))/(n*pi/(Np+1.0d0)))**ikernel !Lanczos kernel
+      else if(ikernel.eq.0) then
+        gn(n)=dble(Np-n+1)/dble(Np+1) !Fej\'er kernel
       else
         if(inode_tot.eq.1) write(6,*) 
      &   "warning: ikernel is not recognized, no kernel is used."
         gn(n)=1.d0
       endif
       enddo
-
-      do m=1,nblock_band_mx
-        s=0.d0
-        do i=1,ng_n
-          s=s+cdabs(ug_n_bp(i,m))**2
-        enddo
-        call global_sumr(s)
-        s=1.d0/dsqrt(s*vol)
-        do i=1,ng_n
-          ug_n_bp(i,m)=s*ug_n_bp(i,m)
-        enddo
-      enddo
-
-
-       if(ipsp_all.eq.1) then
-       call orthogonal_choleskyBP(ug_n_bp,mg_nx,nblock_band_mx,
-     &      ng_n,hh)    
-       else
-       call orthogonal_choleskyBP2(ug_n_bp,sug_m,mg_nx,
-     &      nblock_band_mx,ng_n,hh,1)      ! iflag=1, sug_m is rotated    
-       endif
 
       do 3000 nint=1,nline
 ****************************************
@@ -190,36 +167,62 @@ cc     yg->xg zg->yg
 
        call mpi_barrier(MPI_COMM_WORLD,ierr)
 
-       if(ipsp_all.eq.1) then
-       call orthogonal_choleskyBP(ug_n_bp,mg_nx,nblock_band_mx,
-     &      ng_n,hh)    
-       else
-       call orthogonal_choleskyBP2(ug_n_bp,sug_m,mg_nx,
-     &      nblock_band_mx,ng_n,hh,1)      ! iflag=1, sug_m is rotated    
+****************************************
+cc     orthonormalize ug using Cholesky decompostion
+***************************************
+C        if(ipsp_all.eq.1) then
+C        call orthogonal_choleskyBP(ug_n_bp,mg_nx,nblock_band_mx,
+C      &      ng_n,hh)    
+C        else
+C        call orthogonal_choleskyBP2(ug_n_bp,sug_m,mg_nx,
+C      &      nblock_band_mx,ng_n,hh,1)      ! iflag=1, sug_m is rotated    
+ 
+C        endif
+       call dot_product_BP(ug_n_bp,ug_n_bp,ng_n,hh)
+       do i=2,mx
+        hh(i,1:i-1)=dcmplx(0.0d0,0.0d0)
+        hh(i,i:mx)=hh(i,i:mx)*vol
+       enddo
+
+       call zpotrf('U',mx,hh,mx,info)
+       if(info.ne.0) then
+        if(inode_tot.eq.1) write(6,*) "Something is wrong with 
+     &    Cholesky decomposition of S. info", info
+       call mpi_abort(MPI_COMM_WORLD,ierr)
        endif
 
+       call ztrtri('U','N',mx,hh,mx,info)
+       if(info.ne.0) then
+        if(inode_tot.eq.1) write(6,*) "Somthing is wrong with inversion 
+     &    of Cholesky factor of S. info", info
+       call mpi_abort(MPI_COMM_WORLD,ierr)
+       endif
+
+       call rotate_wfBP(ug_n_bp,hh,ng_n,mg_nx)
+
+****************************************
+cc     subspace diagonalization
+***************************************
        call Hpsi_comp_AllBandBP(ug_n_bp,xg_n_bp,
      &      nblock_band_mx,ilocal,vr,workr_n,kpt,1,
      &      sug_m,sumdum_m,iislda)
        call dot_product_BP(xg_n_bp,ug_n_bp,ng_n,hh)
-
        call system_czheev('V','U',mx,hh,mst,E_st,workx,
      &      lwork,workrx,info,MPI_COMM_K1)
 
+       E_m(1:nblock_band_mx)=E_st(band_dis(1):band_dis(2))*vol
+
        call rotate_wfBP(ug_n_bp,hh,ng_n,mg_nx)
        call rotate_wfBP(xg_n_bp,hh,ng_n,mg_nx)
-
 c       sumdum_m=dconjg(sumdum_m)
 c       call rotate_wfBP(sumdum_m,hh,mref*natom,mref*natom)
 c       sumdum_m=dconjg(sumdum_m)
-
        if(ipsp_all.eq.2) call rotate_wfBP(sug_m,hh,ng_n,mg_nx)
-       
-       E_st(1:mst)=E_st(1:mst)*vol
-       E_m(1:nblock_band_mx)=E_st(band_dis(1):band_dis(2))
 
+****************************************
+cc     check convergency
+***************************************
        err_m=0.d0
-
        do m=1,nblock_band_mx
        if(ipsp_all.eq.1) then
          do i=1,ng_n
@@ -240,19 +243,24 @@ c       sumdum_m=dconjg(sumdum_m)
        err_m(m)=dsqrt(dabs(tmp_real(m)*vol))
        enddo
 
+       E_st=0.d0
+       E_st(band_dis(1):band_dis(2))=E_m(1:nblock_band_mx)
+       call mpi_allreduce(E_st,dumm,mst,MPI_REAL8,
+     &      MPI_SUM,MPI_COMM_B2,ierr)
+       E_st=dumm
+
        err_st=0.d0
        err_st(band_dis(1):band_dis(2))=err_m(1:nblock_band_mx)
        call mpi_allreduce(err_st,dumm,mst,MPI_REAL8,
      &      MPI_SUM,MPI_COMM_B2,ierr)
        err_st=dumm
        
-       x=1.0d0
-       do m=1,mst
-       if (E_st(m).lt.Ewind(1).or.E_st(m).gt.Ewind(2)) then
+c       do m=1,mst
+c       if (E_st(m).lt.Ewind(1).or.E_st(m).gt.Ewind(2)) then
 c         E_st(m)=(x-x)/(x-x) !make it NaN
-         err_st(m)=0.d0
-       endif
-       enddo
+c         err_st(m)=0.d0
+c       endif
+c       enddo
 
        if(inode_tot.eq.1) then
        write(6,*) "*********************************"
@@ -262,7 +270,7 @@ c         E_st(m)=(x-x)/(x-x) !make it NaN
        write(6,*) "eigen energies, in eV"
        write(6,103) (E_st(i)*27.211396d0, i=1,mx)
        write(6,*) "*********************************"
-      endif
+       endif
 
 ****************************************
 cc     Exit if err_st<=tol for all Ritz values in the target interval.
@@ -288,9 +296,9 @@ cc     Exit if err_st<=tol for all Ritz values in the target interval.
        write(6,*) "*********************************"
       endif
 
-
- 102  format(5(E10.4,3x))
- 103  format(5(f13.8,1x))
+101   format("[",2(f13.8,1x),"]")
+102   format(5(E10.4,3x))
+103   format(5(f13.8,1x))
 
       return
 
